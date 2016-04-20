e70b78b0a07d3ca5c3724d22bd52f4bb12fdb7ed
phy <hongyang@teambition.com>
2016-04-20 11:53:35 +0800

    compile+test+auto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 
-gulp.task('compile', () => {
-	console.log('test')
-    return gulp.src('src/**/.js')
-           .pipe(babel({
-               presets: ['es2015']
-           }))
-          .pipe(gulp.dest('dist'))
+gulp.task('default', () => {
+  return gulp.src('src/**/*.js')
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(gulp.dest('dist'))
 })
 
-gulp.watch('src/**/*.js', ['compile'])
+gulp.watch('src/**/*.js', ['default'])
