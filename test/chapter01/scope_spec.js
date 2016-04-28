68ae1f0b7b661e299907288fc9ef1d3e83bdbd6b
phy <hongyang@teambition.com>
2016-04-28 14:23:05 +0800

    separated watches

--- test/chapter01/scope_spec.js
+++ test/chapter01/scope_spec.js
@@ -689,5 +689,22 @@ describe('scope', function() {
       expect(child.user.name).toBe('Jill')
       expect(parent.user.name).toBe('Jill')
     })
+
+    //separated watches
+    it('does not digest its parent(s)', function() {
+      var parent = new Scope()
+      var child = parent.$new()
+
+      parent.aValue = 'abc'
+      parent.$watch(
+        function(scope) { return scope.aValue },
+        function(newValue, oldValue, scope) {
+          scope.aValueWas = newValue
+        }
+      )
+
+      child.$digest()
+      expect(child.aValueWas).toBeUndefined()
+    })
   })
 })
