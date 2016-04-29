7467701aca0024c2f5422a18b9c6271c2420a0fc
phy <hongyang@teambition.com>
2016-04-29 16:51:04 +0800

    ues phantomjs replace chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -11,7 +11,11 @@ module.exports = function(config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
+    
+    plugins: [
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
@@ -50,6 +54,6 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome']
+    browsers: ['PhantomJS']
   })
 }
