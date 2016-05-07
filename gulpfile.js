2c8c68b116509e084301f3186fb01f95bcc52b3f
phy <hongyang@teambition.com>
2016-05-07 12:27:24 +0800

    add gulp-concat plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
+const concat = require('gulp-concat')
 
 gulp.task('compile', () => {
   return gulp.src('src/**/*.js')
+  .pipe(concat('all.js'))
   .pipe(babel({
     presets: ['es2015']
   }))
